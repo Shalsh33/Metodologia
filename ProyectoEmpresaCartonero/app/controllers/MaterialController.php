0554586f46043ec41a81478c87e34f0d40f54c00
Noelia D. Carrizo <noeliacarrizo22@gmail.com>
2021-11-06 17:44:16 -0300

    se actualiza controlador de Materiales y se agrega MaterialModel para la gestion del ABM materiales. La vista se ensambla despues

--- ProyectoEmpresaCartonero/app/controllers/MaterialController.php
+++ ProyectoEmpresaCartonero/app/controllers/MaterialController.php
@@ -16,9 +16,86 @@ class MaterialController
     /**
      * Imprime la lista de materiales que hay disponibles para el reciclado
      */
-    function mostrarMateriales()
+    function mostrarMateriales($mensaje)
     {
         // actualizo la vista
-        $this->view->show();
+        $this->view->listadoDeMateriales($mensaje);
+    }
+
+    /**
+     * Muestra el formulario de carga de materiales
+     */
+    function agregarMateriales($mensaje){
+
+        $this->view->mostrarFormulario($mensaje);
+    }
+
+    /**
+     * Registra el material en el sistema
+     */
+    function agregarUnMaterial(){
+
+        $nombre = $_POST['nombre'];
+        $descripcion = $_POST['descripcion'];
+        $es_aceptado = $_POST['es_aceptado'];
+
+        // verifico campos obligatorios
+        if (empty($nombre) || empty($descripcion)|| empty($es_aceptado)) {
+            $mensaje = "Debe completar los datos requeridos para la carga de material.";
+            $this->agregarMateriales($mensaje);
+        }else{
+            $id = $this->model->insertarMaterial($nombre, $descripcion, $es_aceptado);
+            if ($id) {
+                $mensaje = "Se cargo el material de manera exitosa";
+            } else {
+                $mensaje = "No se pudo crear el material. Verifique los datos ingresados y vuelva a intentarlo.";
+            }
+            $this->mostrarMateriales($mensaje);
+        } 
+    }
+
+    /**
+     * Registra el material en el sistema
+     */
+    function actualizarUnMaterial(){
+
+        // toma los datos a actualizar ingresados en el formulario
+        $id = $_POST['id_material'];
+        $nombre = $_POST['nombre'];
+        $descripcion = $_POST['descripcion'];
+        $es_aceptado = $_POST['es_aceptado'];
+
+        // verifico campos obligatorios
+        if (empty($nombre) || empty($descripcion)|| empty($es_aceptado)) {
+            $mensaje = "Debe completar los datos requeridos para la carga de material.";
+            $this->mostrarMateriales($mensaje);
+        }else{
+            $idAct = $this->model->actualizarMaterial($nombre, $descripcion, $es_aceptado, $id);
+            if ($idAct) {
+                $mensaje = "Se actualizo de manera exitosa";
+            } else {
+                $mensaje = "No se pudo actualizar el material. Verifique los datos ingresados y vuelva a intentarlo.";
+            }
+            $this->mostrarMateriales($mensaje);
+        } 
+    }
+
+    /**
+     * Elimina el material registrado en el sistema
+     */
+    function eliminarMaterial($id)
+    {
+        if (is_numeric($id)) {
+            $borrado = $this->model->eliminarMaterial($id);
+            if ($borrado) {
+                $mensaje = "El material ha sido eliminad0.";
+            } else {
+                $mensaje = "Ups!!! ... No se pudo borrar el material.";
+            }
+        } else {
+            $mensaje = "No se pudo recuperar material de la base de datos";
+            $borrado = false;
+        }
+        $this->mostrarMateriales($mensaje);
     }
 }
---
+++ ProyectoEmpresaCartonero/app/models/MaterialModel.php
@@ -0,0 +1,58 @@
+<?php
+
+class MaterialModel{
+
+    private $db;
+
+    function __construct() {
+        // 1. Abro la conexión
+        $this->db = $this->connect();
+    }
+
+    private function connect(){
+        // 2. Conexion con la base
+        //$db = new PDO('mysql:host=localhost;'.'dbname=db_reci_coop;charset=utf8', 'root', '');
+        $db = new PDO('mysql:host=localhost;'.'dbname=db_reci_coop;charset=utf8', 'noelia', '');
+        return $db;
+    }
+
+    /**
+     * Registra un nuevo material
+     */
+    private funcion agregarMaterial($nombre, $descripcion, $es_aceptado){
+        
+        $query = $this->db->prepare('
+        INSERT INTO materiales (nombre, descripcion, es_aceptado )
+                VALUES ( ? , ? , ?)');
+
+        $query->execute([$nombre, $descripcion, $es_aceptado]);
+       
+        return $this->db->lastInsertId();
+    }
+
+    /**
+     * Actualiza los datos de un material
+    */
+    private function actualizarMaterial($nombre, $descripcion, $es_aceptado, $id){
+        
+        $query = $this->db->prepare('
+        UPDATE materiales SET nombre = ?, descripcion = ?, es_aceptado = ? 
+            WHERE id = ?');
+        $query->execute([$nombre, $descripcion, $es_aceptado, $id]);
+    }
+
+    /**
+     * Eliminar un material según id pasado como parámetro 
+    */
+    function eliminarMaterial($id)
+    {
+        $query = $this->db->prepare('
+        DELETE FROM materiales where id_material = ?');
+        $query->execute([$id]);
+        // devuelve numero de columnas afectadas a la eliminacion
+        return $query->rowCount();
+    }
+    
+    
+    
+}
